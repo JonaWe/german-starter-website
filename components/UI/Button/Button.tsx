11b5dde6269717f649107f4224611e0b67915b11
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-22 21:30:04 +0100

    fixes font for button

--- components/UI/Button/Button.tsx
+++ components/UI/Button/Button.tsx
@@ -20,7 +20,7 @@ export default function Button({
   useLink,
   href,
 }: ButtonProps) {
-  const extendedClassName = `text-xl py-2 px-4 flex items-center gap-1 text-sand-500 transition duration-150 ${
+  const extendedClassName = `font-bebas text-xl py-2 px-4 flex items-center gap-1 text-sand-500 transition duration-150 ${
     primary
       ? 'bg-rust-500 hover:bg-rust-600'
       : 'bg-background-300 hover:bg-background-400'
