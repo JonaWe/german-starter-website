e0d9b178a64ef884a65f69770f996901aea1a417
Noe <eon.sberk@gmail.com>
2022-03-22 19:04:20 +0100

    text and btn

--- components/Landing/Header.tsx
+++ components/Landing/Header.tsx
@@ -1,17 +1,20 @@
 import useLocalization from "../../hooks/useLocalization";
+import Button from "../UI/Button";
 
 export default function Header() {
   const t = useLocalization();
   return (
-    <header className="w-screen h-screen bg-cover relative flex items-center text-sand-500">
+    <header className="w-screen h-screen bg-cover relative flex items-center text-sand-500 px-24">
       <div className=" absolute z-10 w-10/12 mx-auto block">
-        <h1>German Starter</h1>
-        <p className="sm:max-w-[30%]">{t.headerText}</p>
+        <h2>{t.subHeader}</h2>
+        <h1 className="leading-none">German Starter</h1>
+        <p className="sm:max-w-[30%] mb-6">{t.headerText}</p>
+        <Button text={t.joinBtn} onClick={() => console.log("ok")} primary />
       </div>
       <img
         src="/assets/images/banner_bg.png"
         alt="banner"
-        className="object-cover w-full h-full opacity-70 absolute"
+        className="object-cover w-full h-full opacity-70 absolute inset-0"
       />
     </header>
   );
--- translations/de.ts
+++ translations/de.ts
@@ -1,4 +1,6 @@
 export default {
   test: 'test',
+  subHeader: "Der Server für jederman",
+  joinBtn: "Server beitreten",
   headerText: `sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takim`,
 };
