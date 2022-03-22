28efce755716d72f52f61dcf874e7511bc41e304
Noe <eon.sberk@gmail.com>
2022-03-22 20:50:22 +0100

    section 1

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -15,7 +15,7 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className={`text-xl py-2 px-4 flex items-center gap-1 text-sand-500 transition duration-150 ${
+      className={`text-xl py-2 px-4 flex items-center gap-1 text-sand-500 transition duration-15 ${
         primary
           ? 'bg-rust-500 hover:bg-rust-600'
           : 'bg-background-300 hover:bg-background-400'
