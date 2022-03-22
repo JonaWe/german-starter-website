13439756c0bfdfca09094b5a9ed5bb6aef85165b
Noe <eon.sberk@gmail.com>
2022-03-22 19:04:12 +0100

    txt and btn

--- translations/en.ts
+++ translations/en.ts
@@ -1,4 +1,6 @@
 export default {
   test: "test",
+  subHeader: "The server for everyone",
+  joinBtn: "Join server",
   headerText: `sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.`,
 };
