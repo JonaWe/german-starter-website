ea23871931f7dd466efef5004dbea2f047444138
Noe <eon.sberk@gmail.com>
2022-03-23 20:54:11 +0100

    responsive header

--- components/Landing/Header.tsx
+++ components/Landing/Header.tsx
@@ -4,10 +4,10 @@ import JoinButton from '../Buttons/JoinButton';
 export default function Header() {
   const t = useLocalization();
   return (
-    <header className="w-screen h-screen bg-cover relative flex items-center text-sand-500 px-24">
+    <header className="w-screen h-screen bg-cover relative flex items-center text-sand-500 sm:px-24 p-5">
       <div className=" absolute z-10 w-10/12 mx-auto block">
         <h2>{t.subHeader}</h2>
-        <h1 className="leading-none">German Starter</h1>
+        <h1 className="leading-none sm:text-[10rem] text-8xl">German Starter</h1>
         <p className="sm:max-w-[30%] mb-6">{t.headerText}</p>
         <JoinButton />
       </div>
