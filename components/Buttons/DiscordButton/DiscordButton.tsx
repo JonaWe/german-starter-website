14840ba95b15f59dc633aeb14dd5a40ea228b0c6
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-23 16:46:58 +0100

    removes unused code

--- components/Buttons/DiscordButton/DiscordButton.tsx
+++ components/Buttons/DiscordButton/DiscordButton.tsx
@@ -1,14 +1,8 @@
-import { useEffect } from "react";
-import useServerConfig from "../../../hooks/useServerConfig";
-import Button from "../../UI/Button";
+import useServerConfig from '../../../hooks/useServerConfig';
+import Button from '../../UI/Button';
 
 export default function CopyButton() {
   const [config] = useServerConfig();
-
-  useEffect(() => {
-    console.log(config);
-    
-  }, [config])
 
   return (
     <Button text="Discord" useLink href={config?.discordUrl}>
