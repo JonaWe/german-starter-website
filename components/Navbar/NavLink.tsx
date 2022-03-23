5e20ba362ac93ee1bdc28aabce5485246501909b
Noe <eon.sberk@gmail.com>
2022-03-23 08:57:02 +0100

    center external icon

--- components/Navbar/NavLink.tsx
+++ components/Navbar/NavLink.tsx
@@ -13,7 +13,7 @@ export default function NavLink({ href, text, externalLink }: NavLinkProps) {
       <Link href={href}>
         <a className="font-bebas flex items-center gap-1 text-3xl [text-shadow:0_4px_8px_rgba(0,0,0,0.12)]">
           {text}
-          {externalLink && <HiOutlineExternalLink className="text-xl" />}
+          {externalLink && <HiOutlineExternalLink className="text-xl translate-y-[-0.11rem]" />}
         </a>
       </Link>
     </li>
