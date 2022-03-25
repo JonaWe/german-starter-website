95d0d04996a32dc59f83fc00c30619a0d6ad6e69
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-25 14:29:56 +0100

    adds news item component

--- pages/news.tsx
+++ pages/news.tsx
@@ -1,18 +1,69 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import NewsItem from '../components/News/NewsItem';
 import PageContent from '../components/PageContent';
 import PageHeader from '../components/PageHeader';
 import useLocalization from '../hooks/useLocalization';
 
-const News: NextPage = () => {
+interface NewsItemContent {
+  title: string;
+  content: string;
+}
+interface NewsItem {
+  de: NewsItemContent;
+  en: NewsItemContent;
+  releaseDate: string;
+}
+
+interface NewsPageProps {
+  newsItems: NewsItem[];
+}
+
+const News: NextPage<NewsPageProps> = ({ newsItems }: NewsPageProps) => {
   const t = useLocalization();
+  const { locale } = useRouter();
   return (
     <>
       <PageHeader imageURL="/assets/images/news_banner.jpg">
         <h1>{t.newsPage.title}</h1>
       </PageHeader>
-      <PageContent>News</PageContent>
+      <PageContent>
+        {(!newsItems || newsItems.length === 0) && <p>No News found!</p>}
+        {newsItems &&
+          newsItems.map(({ en, de, releaseDate }, index) => {
+            const { title, content } = locale === 'de' ? de : en;
+            return (
+              <NewsItem
+                title={title}
+                content={content}
+                releaseDate={JSON.parse(releaseDate)}
+                key={index}
+              />
+            );
+          })}
+      </PageContent>
     </>
   );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      newsItems: [
+        {
+          de: {
+            title: 'Wartungsarbeiten',
+            content: `# Wartungsarbeiten am 28.03.2022\nAm Montag Morgen den **28.03.2022** um **06:05 Uhr** gibt es Wartungsarbeiten. Dies wird ca 5 bis 10 minuten in Anspruch nehmen.`,
+          },
+          en: {
+            title: 'Maintenance',
+            content: `# Maintenance on 28.03.2022\nThere will be some maintenance on Monday **28.03.2022** at **06:05 CET** this will take about 5 to 10 minutes.`,
+          },
+          releaseDate: JSON.stringify(new Date()),
+        },
+      ],
+    },
+  };
+}
 
 export default News;
