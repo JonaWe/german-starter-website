98d09e4faac66a1dc3e7ea70c2b92033d0abb3ad
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-26 00:41:18 +0100

    adds a default layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Header from '../components/Landing/Header';
 import Benefits from '../components/Landing/Sections/Benefits/Benefits';
 import Community from '../components/Landing/Sections/Community';
 import Server from '../components/Landing/Sections/Server';
-import Navbar from '../components/Navbar';
 import PageContent from '../components/PageContent';
 import useLocalization from '../hooks/useLocalization';
 
