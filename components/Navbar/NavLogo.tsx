c2456aa9b1d760a183d262f329f36f03736e67bc
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-25 15:15:09 +0100

    clicking on the nav logo now directs to the home page

--- components/Navbar/NavLogo.tsx
+++ components/Navbar/NavLogo.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function NavLogo() {
   return (
-    <Image
-      src="/assets/images/logo.png"
-      height={50}
-      width={50}
-      alt="German Starter Logo"
-    />
+    <Link href="/">
+      <a>
+        <Image
+          src="/assets/images/logo.png"
+          height={50}
+          width={50}
+          alt="German Starter Logo"
+        />
+      </a>
+    </Link>
   );
 }
