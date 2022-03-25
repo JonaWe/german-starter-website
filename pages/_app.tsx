fbe33d5e1fb422fec1790aa2c7c61c861a9ffbbd
Jona Wessendorf <jonawessendorf@gmail.com>
2022-03-25 21:32:57 +0100

    adds next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 
+import { NextSeo } from 'next-seo';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Layout from '../components/Layout';
@@ -10,6 +11,25 @@ const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <NextSeo
+        title="German Starter Server"
+        description="German Starter Server"
+        canonical="https://www.german-starter.de"
+        openGraph={{
+          url: 'https://www.german-starter.de',
+          title: 'German Starter Server',
+          description: 'German Starter Server',
+          images: [
+            {
+              url: 'https://www.german-starter.de/assets/images/banner_bg.png',
+              width: 1920,
+              height: 1080,
+              alt: 'German Starter Banner',
+              type: 'image/png',
+            },
+          ],
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
