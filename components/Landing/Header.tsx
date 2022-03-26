50dc944576df4dc8fe5a8019918a75ff9e35738f
Noe <eon.sberk@gmail.com>
2022-03-26 14:06:27 +0100

    gradient

--- components/Landing/Header.tsx
+++ components/Landing/Header.tsx
@@ -4,7 +4,7 @@ import JoinButton from '../Buttons/JoinButton';
 export default function Header() {
   const t = useLocalization();
   return (
-    <header className="relative flex h-screen w-screen items-center bg-cover p-5 text-sand-500 sm:px-24">
+    <header className="relative flex h-screen w-screen items-center bg-cover p-5 text-sand-500 sm:px-24 after:absolute after:top-0 after:right-0 after:left-0 after:h-[35vh] after:bg-gradient-to-b after:from-black/20">
       <div className=" absolute z-10 mx-auto block w-10/12">
         <h2>{t.subHeader}</h2>
         <h1 className="leading-none">German Starter</h1>
