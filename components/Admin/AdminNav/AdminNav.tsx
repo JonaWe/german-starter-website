20acb31ce7070b653b2c796eaf1fa42f021c4c2d
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-02 01:28:10 +0200

    adds logout button and language select to admin nav

--- components/Admin/AdminNav/AdminNav.tsx
+++ components/Admin/AdminNav/AdminNav.tsx
@@ -1,14 +1,23 @@
 import Link from 'next/link';
 
+import LanguageSelection from '../../Navbar/LanguageSelection';
 import AdminNavLinks from './AdminNavLinks';
 
 export default function AdminNav() {
   return (
-    <div className="h-screen w-80 bg-background-600 pt-8 flex flex-col gap-8">
-      <Link href="/">
-        <a className="px-5">Exit Admin Panel</a>
-      </Link>
-      <AdminNavLinks />
+    <div className="h-screen w-80 bg-background-600 py-8 flex flex-col justify-between">
+      <div className="flex flex-col gap-8">
+        <Link href="/">
+          <a className="px-5">Exit Admin Panel</a>
+        </Link>
+        <AdminNavLinks />
+      </div>
+      <span className="flex items-center justify-between px-5 ">
+        <Link href="/logout">
+          <a className="text-xl">Logout</a>
+        </Link>
+        <LanguageSelection />
+      </span>
     </div>
   );
 }
