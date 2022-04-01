22f3318447e898c0ff789787bb8c5489d1eae9ce
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-02 00:53:34 +0200

    adds accessebility to input

--- components/MarkdownEditor/MarkdownEditorInputItem.tsx
+++ components/MarkdownEditor/MarkdownEditorInputItem.tsx
@@ -26,9 +26,17 @@ export default function MarkdownEditorInputItem<T extends keyof FormInput>({
       <label className="text-sand-500 pb-1 mt-4" htmlFor={input}>
         {title}
       </label>
-      {React.createElement(as, { ...register(input), className, id: input })}
+      {React.createElement(as, {
+        ...register(input),
+        className,
+        id: input,
+        'aria-describedby': `${input}-error`,
+        'aria-invalid': errors[input] !== undefined,
+      })}
       {errors[input] && (
-        <span className="text-red-500">{errors[input].message}</span>
+        <span className="text-red-500" id={`${input}-error`}>
+          {errors[input].message}
+        </span>
       )}
     </>
   );
