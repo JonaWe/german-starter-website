b53e2155803a7f94d9c971c641b556e1f80e6208
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-01 23:17:28 +0200

    adds error messages to markdown editor

--- components/MarkdownEditor/MarkdownEditor.tsx
+++ components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
 import useDebounce from '../../hooks/useDebounce';
@@ -10,31 +10,48 @@ import NewsItem from '../News/NewsItem';
 
 const schema = yup
   .object({
-    title: yup.string().required().min(5).max(30),
-    content: yup.string().required().min(10).max(10000),
+    title: yup.string().required('Title is required').min(5).max(35),
+    content: yup.string().required('Content is required').min(10).max(10000),
   })
   .required();
 
+interface FormInput {
+  title: string;
+  content: string;
+}
+
 export default function MarkdownEditor() {
-  const { handleSubmit, register, watch } = useForm({
+  const {
+    handleSubmit,
+    register,
+    watch,
+    formState: { errors },
+  } = useForm<FormInput>({
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = handleSubmit((data) => {
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
     console.log(data.content, data.title);
-  });
+  };
 
   return (
     <div className="w-full h-full grid grid-cols-2">
-      <form className="flex flex-col px-8" onSubmit={onSubmit}>
+      <form className="flex flex-col px-8" onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="title">Title</label>
         <input id="title" {...register('title')} />
+        {errors.title && (
+          <span className="text-red-500">{errors.title.message}</span>
+        )}
         <label htmlFor="content">Content</label>
         <textarea
           id="content"
           className="resize-none"
           {...register('content')}
         />
+        {errors.content && (
+          <span className="text-red-500">{errors.content.message}</span>
+        )}
+        <input type="submit" />
       </form>
       <NewsItem
         title={watch('title') || 'Untitled'}
