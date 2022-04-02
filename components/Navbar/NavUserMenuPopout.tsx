c0e8cc59244354337461a1e000f02becac555170
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-02 02:13:35 +0200

    adds profile link to nav popout to access profile

--- components/Navbar/NavUserMenuPopout.tsx
+++ components/Navbar/NavUserMenuPopout.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link';
 
 export default function NavUserMenuPopout() {
   return (
-    <div className="bg-background-800/95 py-4 px-6 mt-2">
+    <div className="bg-background-800/95 py-4 px-6 mt-2 flex flex-col gap-2">
+      <Link href="/user">
+        <a>Profile</a>
+      </Link>
       <Link href="/admin">
         <a>Admin</a>
       </Link>
