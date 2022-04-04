93cdb3b65c02991823672ce35e7b4193877798b4
Noe <eon.sberk@gmail.com>
2022-04-04 19:25:23 +0200

    moved author styles

--- components/Admin/AdminNews/Author.tsx
+++ components/Admin/AdminNews/Author.tsx
@@ -8,6 +8,10 @@ export default function Author({ id }: AuthorProps) {
   const [author] = usePublicUser(id);
 
   console.log(author);
-  
-  return <>{author?.displayName}</>;
+
+  return (
+    <span className="text-xs px-2.5 py-1.5 bg-sand-500/10 rounded-full border border-sand-500/50 text-sand-500/50 uppercase mr-2">
+      {author?.displayName}
+    </span>
+  );
 }
--- components/Admin/AdminNews/Cells/AuthorsCell.tsx
+++ components/Admin/AdminNews/Cells/AuthorsCell.tsx
@@ -8,7 +8,7 @@ export default function AuthorsCell({ value: authors }: AuthorsCellProps) {
   return (
     <>
       {authors.map((id) => (
-        <span key={id} className="text-xs px-2.5 py-1.5 bg-gray-500/10 rounded-full border border-gray-500 text-gray-500 uppercase mr-2">
+        <span key={id}>
           <Author id={id} />
         </span>
       ))}
