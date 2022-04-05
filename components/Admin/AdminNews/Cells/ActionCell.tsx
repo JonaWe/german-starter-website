13bd60e9f1e69704b3e004a57f34133234a03b4c
Noe <eon.sberk@gmail.com>
2022-04-05 19:21:24 +0200

    Working tooltip

--- components/Admin/AdminNews/Cells/ActionCell.tsx
+++ components/Admin/AdminNews/Cells/ActionCell.tsx
@@ -35,9 +35,7 @@ export default function ActionCell({ value: id, row }: ActionCellProps) {
       <Link href={`/admin/news/${id}`}>
         <a>
           <Tooltip text="Edit">
-            <HiPencil
-              className="fill-sand-500/75 hover:fill-sand-500 text-2xl transition hover:cursor-pointer"
-            />
+            <HiPencil className="fill-sand-500/75 hover:fill-sand-500 text-2xl transition hover:cursor-pointer" />
           </Tooltip>
         </a>
       </Link>
@@ -50,10 +48,10 @@ export default function ActionCell({ value: id, row }: ActionCellProps) {
         </Tooltip>
       ) : (
         <Tooltip text="Publish">
-          <HiEye
-            className="fill-sand-500/75 hover:fill-sand-500 text-2xl transition hover:cursor-pointer"
-            onClick={changePublished}
-          />
+            <HiEye
+              className="fill-sand-500/75 hover:fill-sand-500 text-2xl transition hover:cursor-pointer"
+              onClick={changePublished}
+            />
         </Tooltip>
       )}
     </span>
