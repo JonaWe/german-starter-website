5017491a012996317a54ef5542b2b10391e91883
Noe <eon.sberk@gmail.com>
2022-04-06 16:04:30 +0200

    fiexed not showing comments if clicked show more

--- components/News/CommentSection/NewsComments.tsx
+++ components/News/CommentSection/NewsComments.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 
 import {
   DocumentData,
+  Query,
   collection,
   doc,
   limit,
   orderBy,
   query,
   startAfter,
+  startAt,
 } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 
@@ -28,11 +30,10 @@ export default function NewsComments({ id }: NewsCommentsProps) {
   const [showAll, setShowAll] = useState(false);
   const t = useLocalization();
 
-  const q = query(
-    commentsRef,
-    orderBy('createdAt', 'desc'),
-    showAll ? startAfter(null) : limit(3)
-  );
+  let q: Query;
+
+  if (showAll) q = query(commentsRef, orderBy('createdAt', 'desc'));
+  else q = query(commentsRef, orderBy('createdAt', 'desc'), limit(3));
 
   const [data, loading, error] = useCollection(q);
 
@@ -46,15 +47,14 @@ export default function NewsComments({ id }: NewsCommentsProps) {
       <div className="relative">
         <div className="border-l-4 absolute h-full translate-x-6 border-background-150 z-[1]" />
         <div className="relative z-[2]">
-          {comments &&
-            comments.map(({ author, createdAt, comment }) => {
-              return (
-                <NewsComment uid={author} date={createdAt} comment={comment} />
-              );
-            })}
+          {comments.map(({ author, createdAt, comment }) => {
+            return (
+              <NewsComment uid={author} date={createdAt} comment={comment} />
+            );
+          })}
         </div>
       </div>
-      {comments.length !== 0 ? (
+      {comments ? (
         <button
           className="text-rust-500/60 hover:text-rust-500 transition"
           onClick={() => setShowAll(!showAll)}
@@ -64,7 +64,9 @@ export default function NewsComments({ id }: NewsCommentsProps) {
             : t.newsPage.comments.showMore}
         </button>
       ) : (
-        <p className="text-sand-500/30 text-xs">{t.newsPage.comments.noComments}</p>
+        <p className="text-sand-500/30 text-xs">
+          {t.newsPage.comments.noComments}
+        </p>
       )}
     </>
   );
