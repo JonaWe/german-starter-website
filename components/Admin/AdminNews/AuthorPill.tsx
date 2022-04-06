ca0b92cdb7f585747e5b40cbd58395ba67cd58df
Noe <eon.sberk@gmail.com>
2022-04-06 11:29:53 +0200

    removed log

--- components/Admin/AdminNews/AuthorPill.tsx
+++ components/Admin/AdminNews/AuthorPill.tsx
@@ -7,8 +7,6 @@ interface AuthorProps {
 export default function AuthorPill({ id }: AuthorProps) {
   const [author] = usePublicUser(id);
 
-  console.log(author);
-
   return (
     <span className="text-xs px-2.5 py-1.5 bg-sand-500/10 rounded-full border border-sand-500/50 text-sand-500/50 uppercase mr-2">
       {author?.displayName || author?.email}
