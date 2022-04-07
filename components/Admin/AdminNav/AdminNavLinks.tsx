5306bd6a8b8e351a37a897976e62d481e86ee7e6
Noe <eon.sberk@gmail.com>
2022-04-07 16:07:17 +0200

    Dashboard link in nav

--- components/Admin/AdminNav/AdminNavLinks.tsx
+++ components/Admin/AdminNav/AdminNavLinks.tsx
@@ -1,10 +1,15 @@
-import { HiAnnotation, HiNewspaper, HiUser } from 'react-icons/hi';
+import { HiAnnotation, HiChartBar, HiNewspaper, HiUser } from 'react-icons/hi';
 
 import AdminNavLink from './AdminNavLink';
 
 export default function AdminNavLinks() {
   return (
     <div className="">
+      <AdminNavLink
+        name="Dashboard"
+        link="/"
+        icon={<HiChartBar className="text-xl" />}
+      />
       <AdminNavLink name="Users" icon={<HiUser className="text-xl" />} />
       <AdminNavLink name="News" icon={<HiNewspaper className="text-xl" />} />
       <AdminNavLink
