39a6be93ab03fcf400cd64fa181450ca622b0a67
Jona Wessendorf <jonawessendorf@gmail.com>
2022-04-10 19:34:51 +0200

    adds server team section to home page

--- components/Landing/Sections/Team/Team.tsx
+++ components/Landing/Sections/Team/Team.tsx
@@ -1,23 +1,74 @@
-import Image from 'next/image';
-
 import { Parallax } from 'react-parallax';
 
 import useLocalization from '../../../../hooks/useLocalization';
+import Badge from '../../../UI/Badge';
+import TeamGroup from './TeamGroup';
+
+const GROUP_DEVELOPER = [
+  {
+    name: 'Sumis',
+    image: '/assets/team_avatars/sumis.webp',
+    link: 'https://github.com/Sumis34',
+  },
+  {
+    name: 'Jona',
+    image: '/assets/team_avatars/jona.jpg',
+    link: 'https://github.com/JonaWe',
+  },
+];
+
+const GROUP_ADMINS = [
+  {
+    name: 'Stanley Tylon',
+    image: '/assets/team_avatars/stanley_tylon.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'Banana Joe',
+    image: '/assets/team_avatars/banana_joe.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+];
+
+const GROUP_OWNER = [
+  {
+    name: 'Neo',
+    image: '/assets/team_avatars/neo.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'Python',
+    image: '/assets/team_avatars/python.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'newton',
+    image: '/assets/team_avatars/newton.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+];
 
 export default function Team() {
   const t = useLocalization();
   return (
-    <Parallax
-      bgImage="assets/images/community.png"
-      bgImageAlt="the cat"
-      strength={200}
-      className="w-screen min-h-[60vh] overflow-hidden bg-center bg-cover bg-no-repeat flex justify-center mt-32"
-    >
-      <div className="w-full max-w-screen-2xl">
-        <div className="mx-auto max-w-screen-xl items-center gap-10 sm:m-10 sm:mt-48 md:flex">
-          <p>hallo</p>
-        </div>
+    <section className="relative">
+      <div className="w-full absolute -top-6 left-0 z-10">
+        <Badge className="mx-auto" text="The Server Team" />
       </div>
-    </Parallax>
+      <Parallax
+        bgImage="assets/images/team_banner.png"
+        bgImageAlt="the cat"
+        strength={200}
+        className="w-screen overflow-hidden bg-center bg-cover bg-no-repeat flex justify-center mt-32"
+      >
+        <div className="w-full max-w-screen-2xl">
+          <div className="mx-auto max-w-screen-xl items-center gap-8 lg:gap-12 flex flex-col my-16 lg:my-24">
+            <TeamGroup members={GROUP_OWNER} title={'Owner'} />
+            <TeamGroup members={GROUP_ADMINS} title={'Admins'} />
+            <TeamGroup members={GROUP_DEVELOPER} title={'Developer'} />
+          </div>
+        </div>
+      </Parallax>
+    </section>
   );
 }
