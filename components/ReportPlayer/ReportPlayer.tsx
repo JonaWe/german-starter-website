0cb827f17471f1d8dcbbc8b970153cc98b4f508f
Noe <eon.sberk@gmail.com>
2022-04-10 13:29:16 +0200

    report card

--- components/ReportPlayer/ReportPlayer.tsx
+++ components/ReportPlayer/ReportPlayer.tsx
@@ -1,19 +1,26 @@
-import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
+
+import { AnimatePresence } from 'framer-motion';
 
 import useLocalization from '../../hooks/useLocalization';
 import SimpleListbox from '../UI/Listbox';
 import { Player } from './Interfaces/Player';
 import ReportSteps from './ReportSteps';
+import ReportTypeCard from './ReportTypeCard';
 import SelectPlayer from './SelectPlayer';
 
 export default function ReportPlayer() {
   const t = useLocalization();
 
-
   return (
     <>
-      <ReportSteps />
+      <ReportTypeCard
+        image="/assets/images/oilrig_ak.png"
+        title="Report"
+        description="Report a player"
+        href="/report/player"
+      />
+      {/* <ReportSteps /> */}
     </>
   );
 }
---
+++ components/ReportPlayer/ReportTypeCard.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+interface ReportTypeCardProps {
+  image: string;
+  title: string;
+  description: string;
+  href: string;
+}
+
+export default function ReportTypeCard({
+  image,
+  title,
+  description,
+  href,
+}: ReportTypeCardProps) {
+  return (
+    <Link href={href}>
+      <a className="overflow-hidden bg-background-500 relative cursor-pointer w-64 h-80 p-5 flex items-end hover:shadow-xl transition hover:-translate-y-1 group duration-300">
+        <div className="z-[10] relative">
+          <h2 className="leading-none">{title}</h2>
+          <p className="leading-none text-sm">{description}</p>
+        </div>
+        <img
+          src={image}
+          alt=""
+          className="object-cover absolute inset-0 h-80 group-hover:scale-[101%] scale-100 transition duration-300"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-background-700 translate-y-0 group-hover:translate-y-12 transition-all duration-300" />
+      </a>
+    </Link>
+  );
+}
