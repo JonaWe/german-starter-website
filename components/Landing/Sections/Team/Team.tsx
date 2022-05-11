6bab4d628ccd09c8dc2739d7cc7f2c623f7161ce
Noe <eon.sberk@gmail.com>
2022-05-11 18:14:24 +0200

    export team data to seperate file

--- components/Landing/Sections/Team/Team.tsx
+++ components/Landing/Sections/Team/Team.tsx
@@ -1,52 +1,13 @@
 import { Parallax } from 'react-parallax';
 
+import {
+  GROUP_ADMINS,
+  GROUP_DEVELOPER,
+  GROUP_OWNER,
+} from '../../../../data/Team';
 import useLocalization from '../../../../hooks/useLocalization';
 import Badge from '../../../UI/Badge';
 import TeamGroup from './TeamGroup';
-
-const GROUP_DEVELOPER = [
-  {
-    name: 'Sumis',
-    image: '/assets/team_avatars/sumis.webp',
-    link: 'https://github.com/Sumis34',
-  },
-  {
-    name: 'Jona',
-    image: '/assets/team_avatars/jona.jpg',
-    link: 'https://github.com/JonaWe',
-  },
-];
-
-const GROUP_ADMINS = [
-  {
-    name: 'Stanley Tylon',
-    image: '/assets/team_avatars/stanley_tylon.png',
-    link: 'https://steamcommunity.com/profiles/76561197961262339',
-  },
-  {
-    name: 'Banana Joe',
-    image: '/assets/team_avatars/banana_joe.png',
-    link: 'https://steamcommunity.com/profiles/76561197961262339',
-  },
-];
-
-const GROUP_OWNER = [
-  {
-    name: 'Neo',
-    image: '/assets/team_avatars/neo.png',
-    link: 'https://steamcommunity.com/profiles/76561197961262339',
-  },
-  {
-    name: 'Python',
-    image: '/assets/team_avatars/python.png',
-    link: 'https://steamcommunity.com/profiles/76561197961262339',
-  },
-  {
-    name: 'newton',
-    image: '/assets/team_avatars/newton.png',
-    link: 'https://steamcommunity.com/profiles/76561197961262339',
-  },
-];
 
 export default function Team() {
   const t = useLocalization();
---
+++ data/Team.ts
@@ -0,0 +1,45 @@
+const GROUP_DEVELOPER = [
+  {
+    name: 'Sumis',
+    image: '/assets/team_avatars/sumis.webp',
+    link: 'https://github.com/Sumis34',
+  },
+  {
+    name: 'Jona',
+    image: '/assets/team_avatars/jona.jpg',
+    link: 'https://github.com/JonaWe',
+  },
+];
+
+const GROUP_ADMINS = [
+  {
+    name: 'Stanley Tylon',
+    image: '/assets/team_avatars/stanley_tylon.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'Banana Joe',
+    image: '/assets/team_avatars/banana_joe.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+];
+
+const GROUP_OWNER = [
+  {
+    name: 'Neo',
+    image: '/assets/team_avatars/neo.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'Python',
+    image: '/assets/team_avatars/python.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+  {
+    name: 'newton',
+    image: '/assets/team_avatars/newton.png',
+    link: 'https://steamcommunity.com/profiles/76561197961262339',
+  },
+];
+
+export { GROUP_DEVELOPER, GROUP_ADMINS, GROUP_OWNER };
