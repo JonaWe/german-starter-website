271ccaebfc25bd15658bc4d345acab74a36c27e6
Noe <eon.sberk@gmail.com>
2022-05-11 18:20:55 +0200

    Added Link to player cell

--- components/Stats/PlayerStatsTable/Cells/PlayerCell.tsx
+++ components/Stats/PlayerStatsTable/Cells/PlayerCell.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import axios from 'axios';
 import { HiFire } from 'react-icons/hi';
 import Skeleton from 'react-loading-skeleton';
@@ -28,13 +30,17 @@ export default function PlayerCell({ value: name, row }: CellProps) {
 
   return (
     <div className="flex items-center justify-between">
-      <div className="flex gap-2 w-fit min-w-full">
-        <Avatar url={player?.avatar.medium} className="w-10 h-10 flex-none" />
-        <div className="flex flex-col justify-center min-w-[30%]">
-          <p className="font-medium leading-none">{id ? name : <Skeleton />}</p>
-          <p className="text-xs text-sand-500/40">{id || <Skeleton />}</p>
-        </div>
-      </div>
+      <Link href={`/profile/${id}`}>
+        <a className="flex gap-2 w-fit min-w-full">
+          <Avatar url={player?.avatar.medium} className="w-10 h-10 flex-none" />
+          <div className="flex flex-col justify-center min-w-[30%]">
+            <p className="font-medium leading-none">
+              {id ? name : <Skeleton />}
+            </p>
+            <p className="text-xs text-sand-500/40">{id || <Skeleton />}</p>
+          </div>
+        </a>
+      </Link>
       {id && id === data?.player && (
         <Tooltip text={t.stats.playerOfTheDay}>
           <HiFire className="text-2xl fill-rust-500 hover:animate-pulse hover:scale-105 transition-all" />
