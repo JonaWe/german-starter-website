500cd5d07b3423f57e36f6769a0727b5b01c1e2b
Noe <eon.sberk@gmail.com>
2022-05-23 22:59:56 +0200

    show top players in recomended players

--- components/Stats/PlayerPage/FriendsOnServer/RecommendedPlayerCards.tsx
+++ components/Stats/PlayerPage/FriendsOnServer/RecommendedPlayerCards.tsx
@@ -7,9 +7,11 @@ import RecommendedPlayerCard from './RecommendedPlayerCard';
 
 export default function RecommendedPlayerCards({
   steamid,
+  publicProfile = false,
   cardCount,
 }: {
   steamid: string;
+  publicProfile?: boolean;
   cardCount?: number;
 }) {
   const {
@@ -18,44 +20,66 @@ export default function RecommendedPlayerCards({
     isLoading: friendsLoading,
   } = useFriendsOnServer(steamid);
 
-  const { data: topPlayers } = useQuery('topPlayers', () =>
-    fetchPlayersStats(0, 10, null, [
-      {
-        desc: true,
-        id: 'kills',
-      },
-    ])
-  );
+  const randomIndex = Math.floor(Math.random() * (10 + 1));
 
-  const topPlayerWithTag = (topPlayers?.data as players[])?.map(
-    ({ steamid, name }) => {
-      return {
-        steamid,
-        name,
-        tags: [
+  const { data: topPlayers } = useQuery(
+    'topPlayers',
+    () =>
+      fetchPlayersStats(
+        randomIndex,
+        10,
+        null,
+        [
           {
-            name,
-            className: 'green',
+            desc: true,
+            id: 'kills',
           },
-        ],
-      };
+        ]
+      ),
+    {
+      refetchOnWindowFocus: false,
     }
   );
 
-  const cardItems = friendsOnServer
+  const topPlayersWithTag = (topPlayers?.data as players[])
+    ?.filter(
+      //Remove current player form list
+      ({ steamid: topPlayerSteamid }) => String(topPlayerSteamid) !== steamid
+    )
+    ?.map((topPlayer) => {
+      //Reformat data to match RecommendedPlayerCard
+      return {
+        steamid: topPlayer.steamid,
+        name: topPlayer.name,
+        tags: [
+          {
+            name: 'Top Player',
+            color: 'yellow',
+          },
+        ],
+      };
+    });
+
+  const cardItems = publicProfile
     ? friendsOnServer
-    : friendsLoading
-    ? Array(4).fill({})
-    : [];
+      ? friendsOnServer
+      : friendsLoading
+      ? Array(4).fill({})
+      : topPlayersWithTag
+    : topPlayersWithTag;
 
   return (
     <div className="h-52 flex gap-6">
-      {cardItems.slice(0, cardCount || 4).map((friend) => (
+      {cardItems.slice(0, cardCount || 4).map((item) => (
         <RecommendedPlayerCard
-          key={friend.steamid}
-          name={friend.name}
-          steamid={friend.steamid}
-          tags={[{ name: friend.relationship, color: 'green' }]}
+          key={item.steamid}
+          name={item.name}
+          steamid={item.steamid}
+          tags={
+            item.tags
+              ? item.tags
+              : [{ name: item.relationship, color: 'green' }]
+          }
         />
       ))}
     </div>
