95c69172d481ae502ffe0d1cd6355c0b2303e409
Noe <eon.sberk@gmail.com>
2022-06-08 09:10:17 +0200

    removed tooltip

--- components/Stats/CombatLog/Cells/ReasonCell.tsx
+++ components/Stats/CombatLog/Cells/ReasonCell.tsx
@@ -26,22 +26,20 @@ export default function ReasonCell({ value: reason }: CellProps) {
   );
 
   return (
-    <Tooltip text={data?.description || reason}>
-      <motion.a
-        target={'_blank'}
-        className={`px-2 rounded-full transition-all text-sm cursor-pointer font-bold inline-flex items-center gap-1 flex-row`}
-        href={`${FACEPUNCH_URLS.RUST_ITEMS_WIKI_URL}/${formatted_reason}`}
-      >
-        {data && (
-          <span className="w-6 aspect-square rounded-full bg-background-150 inline-flex items-center justify-center">
-            <img
-              src={`${FACEPUNCH_URLS.RUST_ITEMS_FILES_URL}/${formatted_reason}_256.png`}
-              className="w-6 rounded-full inline-block"
-            />
-          </span>
-        )}
-        {data?.title || reason}
-      </motion.a>
-    </Tooltip>
+    <motion.a
+      target={'_blank'}
+      className={`px-2 rounded-full transition-all text-sm cursor-pointer font-bold inline-flex items-center gap-1 flex-row`}
+      href={`${FACEPUNCH_URLS.RUST_ITEMS_WIKI_URL}/${formatted_reason}`}
+    >
+      {data && (
+        <span className="w-6 aspect-square rounded-full bg-background-150 inline-flex items-center justify-center">
+          <img
+            src={`${FACEPUNCH_URLS.RUST_ITEMS_FILES_URL}/${formatted_reason}_256.png`}
+            className="w-6 rounded-full inline-block"
+          />
+        </span>
+      )}
+      {data?.title || reason}
+    </motion.a>
   );
 }
