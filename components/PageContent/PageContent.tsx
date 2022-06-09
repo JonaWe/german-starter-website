cc07588ee57a4b4855831e6645cf5a8a7edc4524
Noe <eon.sberk@gmail.com>
2022-06-09 22:42:05 +0200

    default height 100% for page content

--- components/PageContent/PageContent.tsx
+++ components/PageContent/PageContent.tsx
@@ -5,7 +5,7 @@ interface PageContentProps {
 
 export default function PageContent({ children, className }: PageContentProps) {
   return (
-    <div className="mt-16 flex justify-center">
+    <div className="mt-16 flex justify-center h-full">
       <div className={`w-full max-w-screen-2xl p-5 md:p-0 ${className}`}>
         {children}
       </div>
