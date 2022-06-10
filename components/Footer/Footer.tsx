622e2da5c89e7f89a87cefacacc1af48bbe10149
Noe <eon.sberk@gmail.com>
2022-06-10 19:05:07 +0200

    footer social links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,10 +2,39 @@ import Link from 'next/link';
 
 import { FaDiscord, FaGithub, FaSteam } from 'react-icons/fa';
 
+import useServerConfig from '../../hooks/useServerConfig';
 import NavItems from '../Navbar/NavItems';
 import PageContent from '../PageContent';
 
 export default function Footer() {
+  const [config, setConfig] = useServerConfig();
+
+  const defaultDiscordUrl = 'https://discord.gg/rWqddpwHqF';
+
+  const SOCIAL_LINKS = [
+    {
+      name: 'Discord',
+      icon: (
+        <FaDiscord className="text-xl fill-sand-500/60 hover:fill-sand-500 hover:scale-105 hover:cursor-pointer transition-all" />
+      ),
+      link: config?.discordUrl || defaultDiscordUrl,
+    },
+    {
+      name: 'Github',
+      icon: (
+        <FaGithub className="text-xl fill-sand-500/60 hover:fill-sand-500 hover:scale-105 hover:cursor-pointer transition-all" />
+      ),
+      link: 'https://github.com/JonaWe/german-starter-website',
+    },
+    {
+      name: 'Steam',
+      icon: (
+        <FaSteam className="text-xl fill-sand-500/60 hover:fill-sand-500 hover:scale-105 hover:cursor-pointer transition-all" />
+      ),
+      link: 'https://steamcommunity.com/groups/BHDRML',
+    },
+  ];
+
   return (
     <footer className="w-full mt-40 h-64 bg-background-700">
       <PageContent className="h-full">
@@ -25,16 +54,14 @@ export default function Footer() {
               © Copyright 2022 All Rights Reserved by german-starter.eu, SUMIS
               and Jona.
             </h3>
-            <ul className='flex gap-5'>
-              <li>
-                <FaDiscord />
-              </li>
-              <li>
-                <FaGithub />
-              </li>
-              <li>
-                <FaSteam />
-              </li>
+            <ul className="flex gap-5">
+              {SOCIAL_LINKS.map(({ link, icon }) => (
+                <li>
+                  <a href={link} target="_blank">
+                    {icon}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
