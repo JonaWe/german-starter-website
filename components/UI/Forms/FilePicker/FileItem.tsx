b33fdc48e99c14e55257603b9f140e37690d69fe
Noe <eon.sberk@gmail.com>
2022-06-18 13:25:16 +0200

    made most params for file item optional

--- components/UI/Forms/FilePicker/FileItem.tsx
+++ components/UI/Forms/FilePicker/FileItem.tsx
@@ -7,29 +7,41 @@ export default function FileItem({
   size,
   error,
   onRemove,
+  link,
 }: {
   name: string;
   size: number;
-  onRemove: () => void;
+  link?: string;
+  onRemove?: () => void;
   error?: boolean;
 }) {
   return (
     <li key={name} className="text-sm flex items-center justify-between group">
-      <span
-        className={`flex gap-2 transition-all items-baseline overflow-hidden`}
+      <a
+        href={link ? link : '#'}
+        target={link ? '_blank' : '_self'}
+        className={`${link ? 'cursor-pointer' : 'cursor-default'}`}
       >
-        <p className={`truncate ${error && 'text-red-600/50'}`}>{name}</p>
-        <span className="text-xs opacity-30 mr-5 font-light">
-          {bytes.format(size)}
+        <span
+          className={`flex gap-2 transition-all items-baseline overflow-hidden`}
+        >
+          <p className={`truncate ${error && 'text-red-600/50'}`}>{name}</p>
+          {size && (
+            <span className="text-xs opacity-30 mr-5 font-light">
+              {bytes.format(size)}
+            </span>
+          )}
         </span>
-      </span>
+      </a>
       {error ? (
         <HiBan className={`text-xl fill-red-600 flex-none`} />
       ) : (
-        <HiX
-          onClick={() => onRemove()}
-          className={`text-xl fill-sand-500/50 flex-none cursor-pointer hover:fill-sand-500/80 transition-all`}
-        />
+        onRemove && (
+          <HiX
+            onClick={() => onRemove()}
+            className={`text-xl fill-sand-500/50 flex-none cursor-pointer hover:fill-sand-500/80 transition-all`}
+          />
+        )
       )}
     </li>
   );
