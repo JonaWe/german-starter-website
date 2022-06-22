c229061c7b3f6b9ba769416b78049b76b1e67452
Noe <eon.sberk@gmail.com>
2022-06-22 14:17:11 +0200

    only show allowed sites in admin nav

--- components/Admin/AdminNav/AdminNavLinks.tsx
+++ components/Admin/AdminNav/AdminNavLinks.tsx
@@ -8,9 +8,16 @@ import {
   HiUser,
 } from 'react-icons/hi';
 
+import useRole from '../../../hooks/useRole';
+import isAllowedRole from '../../../lib/firebase/isAllowedRole';
 import AdminNavLink from './AdminNavLink';
 
 export default function AdminNavLinks() {
+  const [role] = useRole(null);
+
+  const isOwner = isAllowedRole(role?.id, 'owner');
+  const isDev = isAllowedRole(role?.id, 'dev');
+
   return (
     <div className="">
       <AdminNavLink
@@ -18,19 +25,25 @@ export default function AdminNavLinks() {
         link="/"
         icon={<HiChartBar className="text-xl" />}
       />
-      <AdminNavLink name="Users" icon={<HiUser className="text-xl" />} />
+      {isOwner && (
+        <AdminNavLink name="Users" icon={<HiUser className="text-xl" />} />
+      )}
       <AdminNavLink name="News" icon={<HiNewspaper className="text-xl" />} />
       <AdminNavLink
         name="Reports"
         icon={<HiAnnotation className="text-xl" />}
       />
+      {isOwner && (
+        <AdminNavLink name="Settings" icon={<HiCog className="text-xl" />} />
+      )}
       <AdminNavLink name="Docs" icon={<HiSupport className="text-xl" />} />
-      <AdminNavLink name="Settings" icon={<HiCog className="text-xl" />} />
-      <AdminNavLink
-        secondary
-        name="Experimental"
-        icon={<HiBeaker className="text-xl" />}
-      />
+      {isDev && (
+        <AdminNavLink
+          secondary
+          name="Experimental"
+          icon={<HiBeaker className="text-xl" />}
+        />
+      )}
     </div>
   );
 }
