eea321d10454a8dd7940d6fbf96de7f335dc6db1
Noé <eon.sberk@gmail.com>
2022-07-02 17:08:12 +0200

    show votes

--- components/Voting/VotingItem.tsx
+++ components/Voting/VotingItem.tsx
@@ -25,7 +25,7 @@ export default function VotingItem({
 }: VotingItemProps) {
   return (
     <div
-      className={`bg-background-700 w-80 flex flex-col transition-all duration-300 hover:scale-105 border-rust-500  ${
+      className={`bg-background-700 w-80 flex flex-col transition-all duration-300 hover:scale-105 border-rust-500 group ${
         checked
           ? 'scale-105 shadow-rust-500/40 shadow-2xl'
           : 'scale-100 hover:shadow-rust-500/10 hover:shadow-2xl'
@@ -35,16 +35,21 @@ export default function VotingItem({
         <div className="absolute bg-gradient-to-t from-background-700 -bottom-1 w-full h-1/3" />
         <img src={imageURL || ''} alt="map" className="w" />
       </div>
-      <div className="px-5 pb-5 pt-1">
-        <h2 className="-mb-2 text-4xl">{name}</h2>
-        <span className="group inline-block">
-          <ArrowButton
-            text={'maashalla mach auf'}
-            onClick={() => {
-              if (mapURL) window.open(mapURL);
-            }}
-          />
-        </span>
+      <div className="px-5 pb-5 pt-1 flex justify-between items-center">
+        <div>
+          <h2 className="-mb-2 text-4xl">{name}</h2>
+          <span className="group inline-block">
+            <ArrowButton
+              text={'maashalla mach auf'}
+              onClick={() => {
+                if (mapURL) window.open(mapURL);
+              }}
+            />
+          </span>
+        </div>
+        <div className="text-4xl translate-y-6 group-hover:translate-y-0 opacity-0 group-hover:opacity-40 transition-all py-3 px-4 bg-background-400/80 border-2">
+          <h2 className="">{votes}</h2>
+        </div>
       </div>
       <span className="bg-background-600 h-2">
         {votes && totalVotes && (
