3d14282ef28fc553503d02973ddf00bd32c251cd
Noé <eon.sberk@gmail.com>
2022-07-03 15:01:57 +0200

    complete header redesign

--- components/Landing/Header.tsx
+++ components/Landing/Header.tsx
@@ -1,46 +1,75 @@
 import Image from 'next/image';
 
-import CountUp from 'react-countup';
 import { Background, Parallax } from 'react-parallax';
-import reactStringReplace from 'react-string-replace';
 
+import useDiscordServerInfo from '../../hooks/useDiscordServerInfo';
 import useLocalization from '../../hooks/useLocalization';
 import JoinButton from '../Buttons/JoinButton';
 import Button from '../UI/Button';
+import SocialProofItem, { Format } from './SocialProofItem';
 
-export default function Header({ playerCount }: { playerCount: number }) {
+export default function Header({
+  playerCount,
+  totalPvPEvents,
+}: {
+  playerCount: number;
+  totalPvPEvents: number;
+}) {
   const t = useLocalization();
+  const { data: discordInfo } = useDiscordServerInfo();
+
+  const socialProofItems = [
+    {
+      name: 'Community',
+      value: playerCount,
+      format: 'compact',
+    },
+    {
+      name: 'Recorded PvP events',
+      value: totalPvPEvents,
+      format: 'compact',
+    },
+    {
+      name: 'Discord members',
+      value: discordInfo.approximate_member_count,
+      format: 'compact',
+    },
+  ];
 
   return (
     <Parallax
       strength={200}
-      className="w-screen h-screen after:absolute after:top-0 after:right-0 after:left-0 after:h-[35vh] after:bg-gradient-to-b after:from-black/20"
+      className="w-screen min-h-screen after:absolute after:top-0 after:right-0 after:left-0 after:h-[35vh] after:bg-gradient-to-b after:from-black/20"
     >
-      <header className="relative flex h-screen w-screen items-center bg-cover p-5 text-sand-500 sm:px-24">
-        <div className=" absolute z-10 mx-auto block w-10/12">
-          <h2>{t.subHeader}</h2>
-          <h1 className="leading-none">
-            {reactStringReplace(t.header, /(\${\w+})/g, () => {
-              return (
-                <span className="relative" key={t.header}>
-                  <span className="opacity-0">{playerCount}</span>
-                  <span className="left-0 absolute">
-                    <CountUp end={playerCount} />
-                  </span>
-                </span>
-              );
-            })}
-          </h1>
-          <p className="mb-6 sm:max-w-[30%]">{t.headerText}</p>
-          <div className="flex gap-5">
-            <JoinButton />
-            <Button text={t.infoBtn} useLink href="#server" />
+      <div>
+        <header className="flex h-screen w-screen items-center p-5 sm:px-0">
+          <div className="w-full text-center flex items-center flex-col">
+            <h1 className="leading-none tracking-widest md:-mb-10">German</h1>
+            <h1 className="leading-none tracking-widest">Starter</h1>
+            <p className="mb-6 sm:max-w-[30%]">{t.headerText}</p>
+            <div className="flex gap-5">
+              <span className="hidden sm:block">
+                <JoinButton />
+              </span>
+              <Button text={t.infoBtn} useLink href="#server" />
+            </div>
+          </div>
+        </header>
+        <div className="md:-mt-24 -mt-12 w-full bg-background-500 flex items-center justify-center md:pt-10 pt-3">
+          <div className="flex justify-between gap-5 sm:gap-20 sm:overflow-hidden overflow-auto">
+            {socialProofItems.map(({ name, value, format }) => (
+              <SocialProofItem
+                name={name}
+                value={value}
+                format={format as Format}
+              />
+            ))}
           </div>
         </div>
-      </header>
+      </div>
       <Background className="w-screen h-screen">
         <Image
-          src="/assets/images/banner_bg_3_alpha_90.png"
+          src="/assets/images/banner_bg_4.png"
           alt="banner"
           layout="fill"
           objectFit="cover"
---
+++ pages/api/discord/index.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import axios from 'axios';
+
+import { db } from '../../../firebase/admin/firebaseAdmin';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const config = await db.doc('config/server').get();
+
+  const id = config.data()?.discordCode;
+
+  const { data } = await axios.get(
+    `https://discord.com/api/v9/invites/${id}?with_counts=true&with_expiration=true`
+  );
+  return res.status(200).send(data);
+}
